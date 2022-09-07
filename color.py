28946e1037350a7e26675459629393d525cf88c2
Joseph Cardoza <josephdeancardoza@gmail.com>
2022-09-07 15:11:11 -0800

    updated amount of colors returns

--- color.py
+++ color.py
@@ -1,13 +1,11 @@
-import colorgram
+import colorgram as cg
 
 
 image = "broncos.jpg"
 
-
-
 #rbg stand for red, green, blud
 def getrgb(path):
-    colors = colorgram.extract(path, 6)
+    colors = cg.extract(path, 2**32) #2**32 will get all as many colors as possible without repeat
     rgb_lis = [color.rgb for color in colors]
 
     return rgb_lis
@@ -15,10 +13,10 @@ def getrgb(path):
 
 #HSL stands for hue, saturation, and lightness.
 def gethsl(path):
-    colors = colorgram.extract(path, 6)
+    colors = cg.extract(path, 2**32)
     hsl_lis = [color.hsl for color in colors]
 
     return hsl_lis
 
-# print(getrgb(image))
+print(getrgb(image))
 # print(gethsl(image))
